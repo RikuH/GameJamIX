b116518a80bd35e2d532af2fd3770923e366e78b
Eero <eerosarkiniemi@gmail.com>
2019-11-07 18:35:42 +0200

    gamemanager & room script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Camera GameCamera;
+    public PlayerMovement player;
+
+    public Room currentRoom;
+    public List<Room> RoomList;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentRoom = RoomList[0];
+        UpdateCameraPosition(currentRoom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void UpdateCameraPosition(Room room)
+    {
+        GameCamera.transform.position = room.RoomCameraPosition.transform.position;
+        GameCamera.transform.rotation = room.RoomCameraPosition.transform.rotation;
+    }
+
+    
+}
