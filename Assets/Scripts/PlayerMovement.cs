9fcffb4ab2e93edad868bc0ac2b581420c91b38c
RikuH <riku_hei@hotmail.com>
2019-11-07 17:26:10 +0200

    juu

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        LookAtCamera();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += Vector3.forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position -= Vector3.forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += Vector3.left * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position -= Vector3.left * Time.deltaTime;
+        }
+    }
+
+    void LookAtCamera()
+    {
+
+    }
+}
