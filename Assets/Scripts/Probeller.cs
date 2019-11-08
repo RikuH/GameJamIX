76e31aa214f7b587a398bc2de8ff01b259657662
Eero <eerosarkiniemi@gmail.com>
2019-11-08 12:39:09 +0200

    kaks ekaa huonetta

---
+++ Assets/Scripts/Probeller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Probeller : MonoBehaviour
+{
+
+    public float turnSpeed = 0.5f;
+    public bool flip = false;
+
+
+    private void FixedUpdate()
+    {
+        if (flip)
+            transform.Rotate(0, turnSpeed, 0);
+
+        else
+            transform.Rotate(0, -1 * turnSpeed, 0);
+    }
+}
