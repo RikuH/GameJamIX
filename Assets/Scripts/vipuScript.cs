2cfdcd8da238eec4691641fc309d7e6f45266d92
RikuH <riku_hei@hotmail.com>
2019-11-08 14:57:13 +0200

    Juue

--- Assets/Scripts/vipuScript.cs
+++ Assets/Scripts/vipuScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class vipuScript : MonoBehaviour
 {
+    [SerializeField] int toggleDelay;
     public Room Room;
     // Start is called before the first frame update
     void Start()
@@ -14,16 +15,26 @@ public class vipuScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void OnTriggerStay(Collider other){
-        if(other.tag == "Player"){
-            if(GameObject.Find("Lonk").GetComponent<PlayerHit>().isHitting){
-                Debug.Log("vipu");
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (GameObject.Find("Lonk").GetComponent<PlayerHit>().isHitting)
+            {
                 Room.ToggleExit();
-                this.transform.rotation = new Quaternion(-40,0,0,0);
+                this.transform.rotation = new Quaternion(-40, 0, 0, 0);
+
+                StartCoroutine(closeDoor());
             }
         }
+    }
+    IEnumerator closeDoor()
+    {
+        yield return new WaitForSeconds(toggleDelay);
+        this.transform.rotation = new Quaternion(-135, 0, 0, 0);
+        Room.ToggleExit();
     }
 }
