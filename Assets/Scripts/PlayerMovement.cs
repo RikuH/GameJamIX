0fea8fb3fe3e76353a0cb1b4562c3e557c140b21
RikuH <riku_hei@hotmail.com>
2019-11-08 12:45:16 +0200

    hihnaa

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,12 +21,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(health <= 0){
+        if (health <= 0)
+        {
             //GG
             Time.timeScale = 0;
         }
         Move();
         LookAtCamera();
+
+
     }
 
     void Move()
@@ -51,39 +54,56 @@ public class PlayerMovement : MonoBehaviour
 
     void LookAtCamera()
     {
-    	// Generate a plane that intersects the transform's position with an upwards normal.
-    	Plane playerPlane = new Plane(Vector3.up, transform.position);
- 
-    	// Generate a ray from the cursor position
-    	Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
- 
-    	// Determine the point where the cursor ray intersects the plane.
-    	// This will be the point that the object must look towards to be looking at the mouse.
-    	// Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
-    	//   then find the point along that ray that meets that distance.  This will be the point
-    	//   to look at.
-    	float hitdist = 0.0f;
-    	// If the ray is parallel to the plane, Raycast will return false.
-    	if (playerPlane.Raycast (ray, out hitdist)) 
-		{
-        	// Get the point along the ray that hits the calculated distance.
-        	Vector3 targetPoint = ray.GetPoint(hitdist);
- 
-        	// Determine the target rotation.  This is the rotation if the transform looks at the target point.
-        	Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
- 
-        	// Smoothly rotate towards the target point.
-        	transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, TurnSpeed * Time.deltaTime);
-		}
+        // Generate a plane that intersects the transform's position with an upwards normal.
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+
+        // Generate a ray from the cursor position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Determine the point where the cursor ray intersects the plane.
+        // This will be the point that the object must look towards to be looking at the mouse.
+        // Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
+        //   then find the point along that ray that meets that distance.  This will be the point
+        //   to look at.
+        float hitdist = 0.0f;
+        // If the ray is parallel to the plane, Raycast will return false.
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            // Get the point along the ray that hits the calculated distance.
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+
+            // Determine the target rotation.  This is the rotation if the transform looks at the target point.
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+            // Smoothly rotate towards the target point.
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, TurnSpeed * Time.deltaTime);
+        }
     }
 
-    void OnCollisionEnter(Collision col){
-        if(col.transform.tag == "Enemy"){
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.tag == "Enemy")
+        {
             health--;
             Debug.Log(health);
 
             this.transform.position -= Vector3.forward * Time.deltaTime * moveSpeed * 10;
+        }
+    }
 
+    void OnCollisionStay(Collision col)
+    {
+        Debug.Log(col.transform.tag);
+        if (col.transform.tag == "rightConvoyer")
+        {
+            Debug.Log("Right");
+            this.transform.position -= Vector3.left * Time.deltaTime * moveSpeed * 2;
+
+        }
+        if (col.transform.tag == "leftConvoy")
+        {
+            Debug.Log("Left");
+            this.transform.position += Vector3.left * Time.deltaTime * moveSpeed * 2;
         }
     }
 
