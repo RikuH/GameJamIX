678730ff5576d6241a79511dc37530bf9c82b734
TaraTorpeedo <54583576+TaraTorpeedo@users.noreply.github.com>
2019-11-08 22:31:20 +0200

    Menu ja gameover

---
+++ Assets/Scripts/GameOverHUD.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverHUD : MonoBehaviour
+{
+    public Button StartGameButton;
+    public Button BackToMenuButton;
+
+    public GameObject GameOverPanel;
+    void GameOver()
+    {   
+        GameOverPanel.SetActive(true);
+    }
+
+    void BackToMenu()
+    {
+        
+        SceneManager.LoadScene("TaritanScene");
+    }
+    void Start()
+    {
+        BackToMenuButton.onClick.AddListener(BackToMenu);
+        StartGameButton.onClick.AddListener(StartGame);
+    }
+
+    void StartGame()
+    {
+        GameOverPanel.SetActive(false);
+        SceneManager.LoadScene("GameScene");
+    }
+
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button StartGameButton;
+    public Button CreditsButton;
+    public Button ExitButton;
+    public Button BackButton;
+
+    public GameObject MainPanel;
+    public GameObject CreditsPanel;
+
+    
+
+    void StartGame()
+    {   
+        MainPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        SceneManager.LoadScene("GameScene");
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("SULKEE");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGameButton.onClick.AddListener(StartGame);
+        CreditsButton.onClick.AddListener(CreditPanel);
+        ExitButton.onClick.AddListener(QuitGame);
+        BackButton.onClick.AddListener(BackButtonToMain);
+    }
+
+    void CreditPanel()
+    {
+        CreditsPanel.SetActive(true);
+        MainPanel.SetActive(false);
+    }
+
+    void BackButtonToMain()
+    {
+        MainPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+    }
+
+}
